1712789a18d2e6c7d7df31c4fbd979a0f2b98f9b
Dmitry Kotov <dmalexkotov@gmail.com>
2022-10-27 00:57:15 +0600

    WatsApp

--- src/managers/WhatsappManager.ts
+++ src/managers/WhatsappManager.ts
@@ -3,6 +3,12 @@ import WarrantyManager from "@src/managers/WarrantyManager";
 
 export default class WhatsappManager {
   static async sendWarranty(warranty: Warranty, phone: string) {
+    console.log({
+      channelId: "3376e1c3-26a6-478b-b964-72bf01cc22cb",
+      chatType: "whatsapp",
+      chatId: phone,
+      contentUri: WarrantyManager.getDownloadLink(warranty.id),
+    })
     const axios = require('axios');
     await axios.post(
       "https://api.wazzup24.com/v3/message",
@@ -14,7 +20,7 @@ export default class WhatsappManager {
       },
       {
         headers: {
-          Authorization: "Bearer c8cf90444023482f909520d454368d27",
+          Authorization: "Bearer fece2f59ddc34dbb8861c1d921a46187",
           "Content-Type": "application/json",
         },
       }
