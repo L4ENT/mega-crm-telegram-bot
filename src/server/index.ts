5e2c69d3d30a7c9dbbb1d37cbad8ca2be9256c60
Dmitry Kotov <dmalexkotov@gmail.com>
2022-10-29 20:03:15 +0600

    Fix calls

--- src/server/index.ts
+++ src/server/index.ts
@@ -12,6 +12,7 @@ server.set('view engine', 'pug');
 server.set('views', path.join(__dirname, 'views'));
 
 server.use('/api', express.json());
+server.use('/api/v1/calls/entry', express.urlencoded());
 server.use(`/bot${config.BOT_TOKEN}`, express.json());
 server.use(`/order-form`, express.urlencoded());
 server.use(`/warranty-form`, express.urlencoded());
